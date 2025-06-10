537c0353dd5190ae0537f3ad879cb71649184d8d
Tanviralam007 <tanviralam451@gmail.com>
2025-06-10 07:47:41 +0600

    practice

---
+++ mod_2/code_2.py
@@ -0,0 +1,40 @@
+# best time to buy and sell stock
+
+# brute force
+def max_profit(prices : list[int]):
+    max_profit = float('-inf') # Initialize to negative infinity
+
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > max_profit:
+                max_profit = profit
+    
+    if max_profit < 0:
+        return 0
+    return max_profit
+
+# optimized solution using two pointers
+def maxProfit(prices: list[int]) -> int:
+    if not prices:
+        return 0
+
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+
+    return [min_price, max_profit]
+
+def main():
+    n = int(input())
+    prices = list(map(int, input().split()))
+    # print(max_profit(prices))
+    # print(maxProfit(prices))
+
+if __name__ == "__main__":
+    main()
